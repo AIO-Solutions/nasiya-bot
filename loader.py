142a021bd5abca2d2b0a75e501d058ef62245c5d
SHermukhammad <shermukhammadtemirov@gmail.com>
2023-11-24 18:20:04 +0500

    I imported postman to loader

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher, types
-from config import API_TOKEN
+from config import API_TOKEN, TELEGRAP_API_TOKEN
 from data.base import Database
 from data.acsess import RAM
 from data.settings import Setting
 from utilits.states import RegistirState, OrderProdactState, UpdateUserData
+from utilits.postman import Postman
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from buttons.defolt import Defolt
 from buttons.inline import InlineButtons
@@ -16,7 +17,7 @@ dp = Dispatcher(bot, storage = storage)
 db = Database('data/database.db')
 ram = RAM('data/database.db')
 setting = Setting('data/setting.json')
-
+postman = Postman(token = TELEGRAP_API_TOKEN)
 
 registir_state = RegistirState()
 order_state = OrderProdactState()
